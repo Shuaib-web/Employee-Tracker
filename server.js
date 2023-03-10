b83dd4e70f5cd5c2051afc19f10d1a62e4b26d14
shuib <shuaib_97@hotmail.co.uk>
2023-03-10 21:28:50 +0000

    displaying results

--- server.js
+++ server.js
@@ -107,4 +107,97 @@ function addEmployee() {
       },
     ])
     .then(updateEmployeeTable);
-}
+} 
+
+async function updateEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "updatedEmployee",
+          message: "Which Employee do you wish to update:",
+          choices: await makeEmployeeArray(),
+        },
+        {
+          type: "list",
+          name: "newRole",
+          message: "What is the employee's new role:",
+          choices: makeRoleArray(),
+        },
+      ])
+      .then(newRole);
+  }
+  
+  function processRequest(answers) {
+    const answer = answers.mainOptions;
+    if (answer === "View all departments") {
+      showTable();
+    } else if (answer === "View all roles") {
+      showJobRoles();
+    } else if (answer === "View all employees") {
+      showEmployees();
+    } else if (answer === "Add a department") {
+      addDepartment();
+    } else if (answer === "Add a role") {
+      addRole();
+    } else if (answer === "Add an employee") {
+      addEmployee();
+    } else if (answer === "Update an employee role") {
+      updateEmployee();
+    } else process.exit();
+  }
+  
+  const startPrompts = () => {
+    openingQuestion()
+      .then(processRequest)
+      .catch((err) => console.error(err));
+  };
+  
+  intro().then(startPrompts);
+  
+  // DATABASE QUERIES
+  function showTable() {
+    db.query(`SELECT * FROM department`, function (err, results) {
+      console.table(`\nDepartments Table:`, results);
+      startPrompts();
+    });
+  }
+  
+  // show job roles
+  
+  function showJobRoles() {
+    db.query(
+      `SELECT job_role.id, 
+              title, 
+              salary, 
+              department.department_name
+       FROM job_role 
+       LEFT JOIN department ON job_role.department_id = department.id`,
+      function (err, results) {
+        console.table(`\nJob Roles Table:`, results);
+        startPrompts();
+      }
+    );
+  }
+  
+  // show employees
+  
+  function showEmployees() {
+    db.query(
+      `SELECT e1.id, 
+              e1.first_name, 
+              e1.last_name, 
+              job_role.title, 
+              job_role.salary, 
+              department.department_name,
+      CONCAT(e2.first_name, ' ', e2.last_name) AS manager
+      FROM employee e1
+      LEFT JOIN job_role ON e1.role_id = job_role.id
+      LEFT JOIN department ON job_role.department_id = department.id
+      LEFT JOIN employee e2 ON e1.manager_id = e2.id;`,
+      function (err, results) {
+        console.table(`\nEmployees Table:`, results);
+        startPrompts();
+      }
+    );
+  }
